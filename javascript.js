24e2304fd6a0ac5763c59627bef18e0d960bfe99
Abdullah Amin <abdullahamin231@gmail.com>
2023-10-03 16:13:13 +0500

    progress

--- javascript.js
+++ javascript.js
@@ -1,13 +1,18 @@
 const container = document.getElementById("container");
+let gridblocks; let grid;
 
+btnReset =  document.createElement("button");
+btnReset.setAttribute("id", "reset");
+btnReset.textContent = "Reset";
+document.body.insertBefore(btnReset,container);
+btnReset.addEventListener("click",resetGrid);
 
 function makeGrid(rows, col){
-
     for(c = 0; c < (rows*col); c++){
         let div = document.createElement("div");
         div.innerText = c + 1;
         div.className = "gridblock";
-        div.style.border = "1px solid black"
+        div.style.border = "0.5px dashed lightblue"
         div.setAttribute("id", `div${c}`);  
         container.appendChild(div);
     }; 
@@ -29,9 +34,29 @@ grid.forEach(block => {
 });
 
 
+function resetGrid(){
+    grid.forEach(element => {
+        container.removeChild(element);
+    });
+    let rowCol = getsize();
+    console.log(rowCol);
+    makeGrid(rowCol[0],rowCol[1]);
+    grid.forEach(block => {
+        block.addEventListener("mouseover", () => {
+            block.style.backgroundColor = "black";
+        });
+        
+    });
+}
 
-grid.forEach(item => {
-    item.addEventListener('mouseover', ()=>{
-        item.style.backgroundColor = "black";
-    })   
-});
+function getsize(){
+    let col = prompt("how many columns? Max 100",16);
+    let row = col;
+    let result=[];
+
+    col = parseInt(col);
+    row = parseInt(row);
+    result.push(col);
+    result.push(row);
+    return result;
+    }
