00db11113c1a4c2cc770b816f6b063c0b5e0766e
Abdullah Amin <abdullahamin231@gmail.com>
2023-10-03 15:01:19 +0500

    nahi hua

--- javascript.js
+++ javascript.js
@@ -1,25 +1,37 @@
 const container = document.getElementById("container");
 
+
 function makeGrid(rows, col){
+
+    for(c = 0; c < (rows*col); c++){
+        let div = document.createElement("div");
+        div.innerText = c + 1;
+        div.className = "gridblock";
+        div.style.border = "1px solid black"
+        div.setAttribute("id", `div${c}`);  
+        container.appendChild(div);
+    }; 
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', col);
-    for(c = 0; c < (rows*col); c++){
-        let cell = document.createElement("div");
-        cell.innerText = c + 1;
-        cell.className = "grid-item";
-        container.appendChild(cell);
-        
-    };
+    document.getElementById("container").style.gridTemplateColumns = `repeat(${col},1fr)`;
+    document.getElementById("container").style.gridTemplateRows= `repeat(${rows},1fr)`;
+
+    let gridblocks = document.getElementsByClassName("gridblock");
+    grid = Array.from(gridblocks);
+    
 };
-
-
-
-const gridItems = document.getElementsByClassName("grid-item");
-
-for (let i = 0; i < gridItems.length; i++) {
-    gridItems[i].addEventListener('mouseenter', function() {
-        gridItems[i].classList.add('hovered');
-    });
-}
 makeGrid(16, 16);
+grid.forEach(block => {
+    block.addEventListener("mouseover", () => {
+        block.style.backgroundColor = "black";
+    });
+    
+});
 
+
+
+grid.forEach(item => {
+    item.addEventListener('mouseover', ()=>{
+        item.style.backgroundColor = "black";
+    })   
+});
